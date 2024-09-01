e427eae3b3d5a1f6793f0e3f28cb9a0f7306f8cf
Herley Antoine <ffast2000@gmail.com>
2024-09-01 11:14:34 -0400

    minor changes

--- app/release/page.tsx
+++ app/release/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <main className="flex min-h-dvh flex-col items-center justify-between pb-24 pt-4 items-center">Page under construction</main>
+    <main className="flex min-h-dvh flex-col justify-center pb-24 pt-4 items-center">Page under construction</main>
   )
 }
 
